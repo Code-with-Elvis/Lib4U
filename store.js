c683cbd25d74d49bfe054a951d7867451047ad8c
Elvis Okumu <okothelvis137@gmail.com>
2025-09-20 22:56:17 +0300

    Install Zustand for state management + set up store.js for Zustand + Install js-cookies for local & session storage

---
+++ store.js
@@ -0,0 +1,41 @@
+import Cookies from "js-cookie";
+import { create } from "zustand";
+
+const useAuth = create((set) => ({
+  user: null,
+  loading: true,
+  login: (user) => set({ user, loading: false }),
+  logout: () => set({ user: null }),
+  setLoading: (loading) => set({ loading }),
+}));
+
+const getInitialTheme = () => {
+  // Check for stored theme
+  const storedTheme = Cookies.get("b_theme");
+
+  if (storedTheme) {
+    return storedTheme;
+  }
+
+  // Fallback to system preference
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  return prefersDarkMode ? "dark" : "light";
+};
+
+const useTheme = create((set) => ({
+  theme: getInitialTheme(),
+  toggleTheme: (theme) => {
+    Cookies.set("b_theme", theme, { expires: 365 });
+    set({ theme });
+  },
+}));
+
+const useAccountModal = create((set) => ({
+  isOpen: false,
+  open: () => set({ isOpen: true }),
+  close: () => set({ isOpen: false }),
+}));
+
+export { useAuth, useTheme, useAccountModal };
