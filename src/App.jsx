811f29d11f2edd591eb5ec0f8808d51980fb0672
Elvis Okumu <okothelvis137@gmail.com>
2025-09-20 21:27:43 +0300

    Install React Router & Set up pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { Button } from "@/components/ui/button";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import {
+  BookDetails,
+  Favorites,
+  Home,
+  Profile,
+  RootLayout,
+  Search,
+  Settings,
+} from "./pages";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="favorites" element={<Favorites />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="settings" element={<Settings />} />
+      <Route path="book/:id" element={<BookDetails />} />
+      <Route path="search" element={<Search />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from "./Home";
+export { default as Favorites } from "./Favorites";
+export { default as Profile } from "./Profile";
+export { default as Settings } from "./Settings";
+export { default as BookDetails } from "./BookDetails";
+export { default as Search } from "./Search";
+export { default as RootLayout } from "./RootLayout";
