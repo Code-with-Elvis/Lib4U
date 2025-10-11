f1816fc854013cfc2cac1006d4e84c3ad30a22f5
Elvis Okumu <okothelvis137@gmail.com>
2025-10-11 08:56:19 +0300

    Feat: Add GoTopBtn component and integrate into Home page.

---
+++ src/components/home/GoTopBtn.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { IoChevronUp } from "react-icons/io5";
+import { Button } from "../ui/button";
+
+const GoTopBtn = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // === HANDLE SCROLL EVENT TO TOGGLE BUTTON VISIBILITY
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 1000) {
+        // === Show button after 1000px of scrolling
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // === SCROLL TO TOP FUNCTION
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // === Smooth scroll animation
+    });
+  };
+
+  return (
+    isVisible && (
+      <Button
+        className="fixed hidden sm:flex right-4 bottom-18 animate-pulse hover:scale-110 transition-transform z-30 shadow-[1px_1px_5px_#aaa]"
+        size="icon"
+        onClick={scrollToTop}
+        aria-label="Scroll to top"
+      >
+        <IoChevronUp />
+      </Button>
+    )
+  );
+};
+export default GoTopBtn;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Categories from "@/components/home/Categories";
 import ChildrenBooks from "@/components/home/ChildrenBooks";
 import FollowUs from "@/components/home/FollowUs";
+import GoTopBtn from "@/components/home/GoTopBtn";
 import Landing from "@/components/home/Landing";
 import NewReleases from "@/components/home/NewReleases";
 
 const Home = () => {
   return (
     <>
+      <GoTopBtn />
       <Landing />
       <div className="lib-container items-start grid max-[1024px]:grid-cols-1 grid-cols-[3fr_1.5fr] gap-10 gap-y-12 my-10">
         {/*=== Left Column ===*/}
